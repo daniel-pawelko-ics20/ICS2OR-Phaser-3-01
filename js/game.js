f33e359e93fcfdaf247825c05245bdf934067e5d
danielpawelkoic <>
2021-04-29 01:03:47 +0000

    done

--- js/game.js
+++ js/game.js
@@ -1,10 +1,28 @@
 /* global Phaser */
 
+// Copyright (c) 2021 Daniel Pawelko All rights reserved
+// 
+// Created by: Daniel Pawelko
+// Created on: April 28 2021
+// This is the Phaser3 configuration file
+
 const config = {
   type: Phaser.AUTO,
   width: 1920,
   height: 1080,
-  backgroundColor: 0x5f6e7a
+  physics: {
+    default: 'arcade',
+    arcade: {
+      debug: true
+    }
+  },
+  // set background color
+  backgroundColor: 0x5f6e7a,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    // we place it in the middle of the page.
+    autoCenter: Phaser.Scale.CENTER_BOTH
+  }
 }
 
 const game = new Phaser.Game(config)
