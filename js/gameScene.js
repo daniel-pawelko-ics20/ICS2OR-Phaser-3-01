308b597f39d8c1974655c2015cba5babac2022a8
danielpawelkoic <>
2021-06-05 03:12:41 +0000

    #15

--- js/gameScene.js
+++ js/gameScene.js
@@ -21,12 +21,12 @@ createAlien () {
   constructor () {
     super({ key: 'gameScene' })
 
-    this.background = null
     this.ship = null
     this.fireMissile = false
     this.score = 0
     this.scoreText = null
     this.scoreTextStyle = { font: '65px Arial', fill: '#ffffff', align: 'center' }
+    this.gameOverTextSyle = { font: '65px Arial', fill: '#ff0000', align: 'center' }
   }
 
   init (data) {
@@ -44,6 +44,7 @@ createAlien () {
     // sound
     this.load.audio('laser', 'assets/laser1.wav')
     this.load.audio('explosion', 'assets/barrelExploding.wav')
+    this.load.audio('bomb', 'assets/bomb.wav')
   }
 
   create (data) {
@@ -71,6 +72,18 @@ createAlien () {
       this.createAlien()
       this.createAlien()
     }.bind(this))
+
+    // Collisions between ship and aliens
+    this.physics.add.collider(this.ship, this.alienGroup, function (shipCollide, alienCollide) {
+      this.sound.play('bomb')
+      this.physics.pause()
+      alienCollide.destroy()
+      shipCollide.destroy()
+      this.gameOverText = this.add.text(1920 / 2, 1080 / 2, 'Game Over!\nClick to play again.', this.gameOverTextSyle).setOrigin(0.5)
+      this.gameOverText.setInteractive({ useHandCursor: true })
+      this.gameOverText.on('pointerdown', () => this.scene.start('gameScene'))
+    }.bind(this))
+
   }
 
   update (time, delta) {
